ede8c5468f6e5f01e66953d0ee21c35c0bc2c98f
JoChuSae <166656451+Hanabruhx3@users.noreply.github.com>
2024-09-25 02:12:16 +0200

    Textausgabe

---
+++ Text-ausgabe/Text-ausgabe/Text-ausgabe.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	// /n macht einen Zeilenumbruch, man braucht \\ um \ anzuzeugen weil \ alleine nur ein escape zeichen ist 
+	printf(" Um diesen Text ausgeben zu koennen,\n muss ich das Escape Zeichen \\ verwenden \n und nach diesem Zeichen, die richtige Anweisung.");
+		return 0;
+}
